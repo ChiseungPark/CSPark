38b01cc439927b98b7624e26328dcaae5819fe32
ChiseungPark <chiseung0729@gmail.com>
2018-07-18 10:55:17 +0900

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 9082;
-
-http.createServer(function(req,res){
-  res.writeHead(200,{'Content-Type':'text/plain'});
-  res.end('Hello World\n');
-}).listen(port,hostname);
-console.log('Server is running at http://127.0.0.1:9082');
